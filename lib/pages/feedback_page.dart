770a338761a5762ec46ad9f0ccdc08c39a84dba5
HarvesYoung <759897298@qq.com>
2025-06-15 17:42:34 +0900

    feedback_page.dart 代码优化改善

--- lib/pages/feedback_page.dart
+++ lib/pages/feedback_page.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:rate/configs/app_config.dart';
@@ -35,9 +34,10 @@ class FeedbackPage extends HookConsumerWidget {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios),
           onPressed: () async {
-            if(pickedImages.length > 0) {
+            if(pickedImages.isNotEmpty) {
               await handleGoBack(context);
             }
+            if(!context.mounted) return;
             Navigator.of(context).pop();
           }
         ),
